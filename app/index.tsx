68e1cf2a7c2420f6ce2b5701a06dd86b6ecff30e
Mohamed-Abukhris <mohamed.s.abukhris@gmail.com>
2025-10-03 18:17:20 +0200

    New features

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
-import {FlatList, View, Pressable, Text, Image} from "react-native";
+import {FlatList, View, Pressable, Text, Image, TouchableOpacity, Animated} from "react-native";
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {offers} from "@/constants";
+import {images, offers} from "@/constants";
 import {Fragment} from "react";
 import cn from 'clsx'
+import ScrollView = Animated.ScrollView;
 
 export default function Index() {
   return (
-      <SafeAreaView>
+      <SafeAreaView className="flex-1 bg-white">
+          <ScrollView>
+          <View className="flex-between flex-row w-full my-5 px-5 ">
+              <View className = "flex-start">
+                  <Text className="small-bold text-primary">DELIVER TO</Text>
+                  <TouchableOpacity className = "flex-center flex-row gap-x-1 mt-0.5">
+                      <Text className ="paragraph-bold text-dark-100"> Libya, Tripoli </Text>
+                      <Image source={images.arrowDown} className="size-3" resizeMode="contain" />
+
+                  </TouchableOpacity>
+              </View>
+              <Text>Cart</Text>
+          </View>
+
           <FlatList data={offers}
                     renderItem={({ item, index }) => {
                         const isEven: boolean = index % 2 == 0;
@@ -15,6 +29,7 @@ export default function Index() {
                             <View>
                                 <Pressable className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')}
                                            style={{backgroundColor: item.color}}
+                                           android_ripple={{ color: "#fffff22"}}
                                 >
                                     {({pressed}) => (
                                         <Fragment>
@@ -22,10 +37,16 @@ export default function Index() {
                                                 <Image source={item.image} className={"size-full"} resizeMode={"contain"} />
                                             </View>
 
-                                            <View className = {"offer-card__info"}>
+                                            <View className = {cn("offer-card__info", isEven ? 'pl-10' : 'pr-10' )}>
                                                 <Text className="h1-bold text-white leading-tight">
                                                     {item.title}
                                                 </Text>
+                                                <Image
+                                                source = {images.arrowRight}
+                                                className = "size-10"
+                                                resizeMode = "contain"
+                                                tintColor="#ffffff"
+                                                />
                                             </View>
                                         </Fragment>
                                     )}
@@ -33,7 +54,9 @@ export default function Index() {
                             </View>
                         )
                     }}
+                    contentContainerClassName="pb-28 px-5"
           />
+          </ScrollView>
       </SafeAreaView>
   );
 }
