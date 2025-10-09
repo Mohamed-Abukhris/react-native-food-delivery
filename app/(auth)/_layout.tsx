45a3ecb7899cacc5204cfd26bd49bcf1ef27f9d2
Mohamed-Abukhris <mohamed.s.abukhris@gmail.com>
2025-10-09 20:41:19 +0200

    Adding the sign in/up functions

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,12 @@
 import {View, Text} from 'react-native'
-import React from 'react'
+import {SafeAreaView} from "react-native-safe-area-context";
+import {Slot} from "expo-router";
 
 export default function _Layout() {
     return (
-        <View>
+        <SafeAreaView>
             <Text>Auth Layout</Text>
-        </View>
+            <Slot />
+        </SafeAreaView>
     )
 }
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,9 +1,12 @@
-import {View, Text} from 'react-native'
+import {View, Text, Button} from 'react-native'
+import {router} from "expo-router";
+
 
 const SignUp = () => {
     return (
         <View>
             <Text>SignUp</Text>
+            <Button title="Sign In" onPress={() => router.push('/sign-in')} />
         </View>
     )
 }
