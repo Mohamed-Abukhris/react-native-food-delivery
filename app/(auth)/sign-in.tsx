beb92ec2ccea4e535df784afe18a109250269727
Mohamed-Abukhris <mohamed.s.abukhris@gmail.com>
2025-10-09 23:12:24 +0200

    feat: improve sign-in experience and components
    - Refined layout in sign-in screen
    - Improved custom UI components
    - Updated .env and lib configuration
    - Synced dependencies and type definitions

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,40 @@
 import {View, Text, Button} from 'react-native'
-import {router} from "expo-router";
+import {Link, router} from "expo-router";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
 
 const SignIn = () => {
+
+
     return (
-        <View>
-            <Text>SignIn</Text>
-            <Button title="Sign Up" onPress={() => router.push('/sign-up')} />
+        <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+            <CustomInput
+                placeholder="Enter your email"
+                value={''}
+                onChangeText={(value: String) => {}}
+                label="Email"
+                keyboardType="email-address"
+            />
+            <CustomInput
+            placeholder="Enter your password"
+            value={''}
+            onChangeText={(value: String) => {}}
+            label="Password"
+            secureTextEntry={true}
+            />
+
+        <CustomButton
+            title="Sign In"
+        />
+
+         <View className="flex justify-center mt-5 flex-row gap-2">
+             <Text className="base-regular text-gray-100">
+                 Don't have an account?
+             </Text>
+             <Link href="/sign-in" className="base-bold text-primary">
+                 Sign Up
+             </Link>
+         </View>
         </View>
     )
 }
