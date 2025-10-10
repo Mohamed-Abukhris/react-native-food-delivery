dd9b0949ae22cf8fda15d83e37470ccb04d2ce87
Mohamed-Abukhris <mohamed.s.abukhris@gmail.com>
2025-10-10 21:36:17 +0200

    chore(secrets): stop tracking lib/appwrite.ts and ignore it

--- lib/appwrite.ts
+++
@@ -1,7 +0,0 @@
-export const appwriteConfig = {
-    endpoint: process.env.APPWRITE_ENDPOINT,
-    projectId: process.env.APPWRITE_PROJECT_ID,
-    platform: "com.react-native-food-delivery",
-    databaseId: '68e809980033047a5f64',
-    userCollectionId: 'user'
-}
